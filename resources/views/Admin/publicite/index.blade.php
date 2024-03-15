76e9322acff2420188dce4c6c251bcabe072496d
dakodiane <dianedako915@gmail.com>
2024-03-15 08:53:02 +0100

    d

--- resources/views/Admin/publicite/index.blade.php
+++ resources/views/Admin/publicite/index.blade.php
@@ -7,18 +7,23 @@
             <div class="col-12">
                 <div class="card mb-4">
                     <div class="card-header pb-0 mb-4 d-flex align-items-center justify-content-between flex-wrap">
-                        <h6>Videos</h6>
+                        <h6>Publicité</h6>
                         <div class="buttons d-flex align-items-center">
-                            <input type="text" name="search" class="form-control mx-2" placeholder="Rechercher">
-                            <button class="btn btn-primary my-0 d-flex me-2" type="submit">
-                                <i class="fa fa-search me-2"></i>
-                                Filter
-                            </button>
-                            <a class="btn btn-success my-0 d-flex" href="{{ route('publicite.create.view') }}">
+                            <form action="{{ route('searchpub') }}" method="GET" class="d-flex">
+                                <input type="text" name="search" class="form-control mx-2" placeholder="Rechercher">
+                                <button class="btn btn-primary my-0 me-2" type="submit">
+                                    <i class="fa fa-search me-2"></i>
+                                    Filter
+                                </button>
+                            </form>
+                            <a class="btn btn-success my-0" href="{{ route('publicite.create.view') }}">
                                 <i class="fa fa-plus me-2"></i>
                                 Ajouter
                             </a>
                         </div>
+
+
+
                     </div>
                     <div class="card-body px-2 pt-0 pb-2">
                         <div class="table-responsive p-0">
@@ -28,7 +33,7 @@
                                         <th>Nom de l'entreprise</th>
                                         <th>Titre</th>
                                         <th>Details</th>
-                                        <th>Status</th>
+                                        <th>Statut</th>
                                         <th>Créé le</th>
                                         <th>Actions</th>
                                     </tr>
@@ -37,17 +42,19 @@
                                     @forelse($publicites as $publicite)
                                     <tr>
                                         <td>{{ $publicite->name }}</td>
-                                        <td>{{ $publicite->offre }}</td>
-                                        <td>{{ $publicite->detail}}</td>
-                                        <td>
-                                            <span class="badge bg-gradient-success">{{ $publicite->statut }}</span>
+                                        <td>{{ Str::limit($publicite->offre, 20) }}</td>
+                                        <td>{{ Str::limit($publicite->detail, 20) }}</td>
+
+                                        <td>    
+                                            <span class="badge bg-gradient-{{ $publicite->statut == 1 ? 'success' : 'danger' }}">
+                                                {{ $publicite->statut == 1 ? 'Actif' : 'Inactif' }}
+                                            </span>
                                         </td>
+
                                         <td>{{ $publicite->created_at }}</td>
                                         <td>
                                             <div class="d-flex align-items-center">
-                                                <a class="btn p-2 d-flex align-items-center me-1" href="{{ route('publicites.details', $publicite->id) }}" title="Voir">
-                                                    <i class="fa fa-eye text-success cursor-pointer"></i>
-                                                </a>
+
                                                 <a class="btn p-2 d-flex align-items-center me-1" href="{{ route('publicites.edit', $publicite->id) }}" title="Modifier">
                                                     <i class="fa fa-edit text-primary cursor-pointer"></i>
                                                 </a>
@@ -57,6 +64,7 @@
                                                         <i class="fa {{ $publicite->statut == '1' ? 'fa-times' : 'fa-check' }} text-{{ $publicite->statut == '1' ? 'danger' : 'success' }} cursor-pointer"></i>
                                                     </button>
                                                 </form>
+
                                             </div>
                                         </td>
                                     </tr>
--- resources/views/components/adminhead.blade.php
+++ resources/views/components/adminhead.blade.php
@@ -99,11 +99,11 @@
             </li>
 
             <li class="nav-item">
-                <a class="nav-link active" href="">
+                <a class="nav-link active" href="{{ route('publicites') }}">
                     <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
                         <i class="ni ni-credit-card text-success text-sm opacity-10"></i>
                     </div>
-                    <span class="nav-link-text ms-1">Publicité</span>
+                    <span class="nav-link-text ms-1">Publicités</span>
                 </a>
             </li>
             <!-- <li class="nav-item">
