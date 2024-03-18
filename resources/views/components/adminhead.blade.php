698dc5995058acef118721a9302afd5106c32c42
dakodiane <dianedako915@gmail.com>
2024-03-18 09:26:10 +0100

    icon publicité

--- resources/views/components/adminhead.blade.php
+++ resources/views/components/adminhead.blade.php
@@ -1,24 +1,24 @@
 <div class="min-height-300 bg-primary position-absolute w-100"></div>
 <!-- Navbar -->
 <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl " id="navbarBlur" data-scroll="false">
-  <div class="container-fluid py-1 px-3">
+    <div class="container-fluid py-1 px-3">
 
-    <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
-      <div class="ms-md-auto pe-md-3 d-flex align-items-center">
-        <div class="input-group">
-          <span class="input-group-text text-body"><i class="fas fa-search" aria-hidden="true"></i></span>
-          <input type="text" class="form-control" id="searchInput" placeholder="Type here..." oninput="search()">
-        </div>
-      </div>
-     
-    </div>
-  </div>
-  <form method="POST" action="{{ route('Deco') }}">
-                @csrf
-                <div class="d-grid">
-                    <button type="submit" class="btn btn-danger btn-block">Déconnexion</button>
+        <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
+            <div class="ms-md-auto pe-md-3 d-flex align-items-center">
+                <div class="input-group">
+                    <span class="input-group-text text-body"><i class="fas fa-search" aria-hidden="true"></i></span>
+                    <input type="text" class="form-control" id="searchInput" placeholder="Type here..." oninput="search()">
                 </div>
-            </form>
+            </div>
+
+        </div>
+    </div>
+    <form method="POST" action="{{ route('Deco') }}">
+        @csrf
+        <div class="d-grid">
+            <button type="submit" class="btn btn-danger btn-block">Déconnexion</button>
+        </div>
+    </form>
 </nav>
 <!-- End Navbar -->
 <aside class="sidenav bg-white navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-4 " id="sidenav-main">
@@ -26,19 +26,19 @@
     <hr class="horizontal dark mt-0">
     <div class="collapse navbar-collapse w-auto" id="sidenav-collapse-main">
         <ul>
-        <li class="nav-item">
-                        <a class="nav-link" href="{{ route('avisadmin') }}">
-                            <span class="ms-2">Avis</span>
-                        </a>
-                </li>
-        <li class="nav-item">
-                        <a class="nav-link" href="{{'utilisateurs'}}">
-                            <span class="ms-2">Visiteurs</span>
-                        </a>
-                    </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('avisadmin') }}">
+                    <span class="ms-2">Avis</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{'utilisateurs'}}">
+                    <span class="ms-2">Visiteurs</span>
+                </a>
+            </li>
         </ul>
         <ul class="navbar-nav">
-      
+
             <li class="nav-item">
                 <a class="nav-link active" href="#">
                     <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
@@ -46,10 +46,10 @@
                     </div>
                     <span class="nav-link-text ms-1">Utilisateurs</span>
                 </a>
-                
+
                 <ul class="nav-dropdown">
-               
-                        
+
+
 
                     <li class="nav-item">
                         <a class="nav-link" href="{{'NousUsers'}}">
@@ -61,7 +61,7 @@
                             <span class="ms-2">Karaoke</span>
                         </a>
                     </li>
-                    
+
                 </ul>
             </li>
 
@@ -74,19 +74,19 @@
                 </a>
             </li>
 
-                <ul class="nav-dropdown">
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('LokNousUsers') }}">
-                            <span class="ms-2">Nous</span>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('LokKaraokeUsers') }}">
-                            <span class="ms-2">Karaoke</span>
-                        </a>
-                    </li>
-                    
-                </ul>
+            <ul class="nav-dropdown">
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('LokNousUsers') }}">
+                        <span class="ms-2">Nous</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('LokKaraokeUsers') }}">
+                        <span class="ms-2">Karaoke</span>
+                    </a>
+                </li>
+
+            </ul>
             </li>
 
             <li class="nav-item">
@@ -101,11 +101,12 @@
             <li class="nav-item">
                 <a class="nav-link active" href="{{ route('publicites') }}">
                     <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-                        <i class="ni ni-credit-card text-success text-sm opacity-10"></i>
+                        <i class="fas fa-ad text-success text-sm opacity-10"></i>
                     </div>
                     <span class="nav-link-text ms-1">Publicités</span>
                 </a>
             </li>
+
             <!-- <li class="nav-item">
                 <a class="nav-link" href="#">
                     <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
@@ -140,7 +141,7 @@
         function resetTimer() {
             clearTimeout(timeout);
             timeout = setTimeout(function() {
-              window.location.href = "{{ route('login') }}";
+                window.location.href = "{{ route('login') }}";
             }, inactivityTimeout);
         }
 
@@ -151,4 +152,3 @@
         resetTimer(); // Initialise le minuteur lors du chargement de la page
     </script>
 </aside>
- 
